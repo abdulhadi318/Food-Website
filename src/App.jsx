a9ae905487b5c18292216a64477c0a089bd2e358
Hammad890 <hammadbro6@gmail.com>
2023-09-21 23:41:10 +0500

    my food app

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Menu from './assets/Menu'
+
+export default function App() {
+  return (
+ <>
+ <div>
+    <Menu/>
+ </div>
+ </>
+  )
+}
---
+++ src/assets/Menu.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Menu() {
+  return (
+    <div>
+         <nav className= '  p-5 text-white  bg-black'>
+<ul className='  text-center  sm:flex  justify-end  sm:space-x-12    sm:font-sans   sm:text-xl sm:pl-5 sm:pr-5 ' >
+    <li><a href="Home"> Home </a></li>
+    <li><a href="Home"> Foods </a></li>
+    <li><a href="Home"> Blog </a></li>
+    <li><a href="Home"> Contact </a></li>
+</ul>
+       
+</nav>
+    </div>
+  )
+}
