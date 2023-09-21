65bc368143f5725e2ea27f6f82816b160fa63d48
Hammad890 <hammadbro6@gmail.com>
2023-09-21 23:55:24 +0500

    asd

--- src/assets/Menu.jsx
+++ src/assets/Menu.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 export default function Menu() {
   return (
     <div>
-         <nav className= '  p-5 text-white  bg-black'>
+         <nav className= '  p-5 text-gray-50  bg-orange-600 '>
+    
 <ul className='  text-center  sm:flex  justify-end  sm:space-x-12    sm:font-sans   sm:text-xl sm:pl-5 sm:pr-5 ' >
     <li><a href="Home"> Home </a></li>
     <li><a href="Home"> Foods </a></li>
